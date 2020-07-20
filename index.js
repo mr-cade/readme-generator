22e1e8f05f14de094a5501104ac95323bba03e1c
mr-cade <cadelpetersen@gmail.com>
2020-07-20 11:35:25 -0600

    inquirer added

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the description of your project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is the table of contents?",
+        name: "contents"
+    },
+    {
+        type: "input",
+        message: "Provide installation instructions.",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "Provide usage instructions.",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Provide license details.",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Provide contribution details.",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Provide testing details",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    }
 ];
 
 // function to write README file
@@ -9,7 +61,8 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    return inquirer.prompt(questions);
+    
 }
 
 // function call to initialize program
